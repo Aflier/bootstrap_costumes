4020618e1d0d30edf478affb35cbd7b094eca687
Simon Kiteley <simon@aflier.com>
2022-08-23 20:38:57 +0100

    Stimulus Controllers

--- lib/bootstrap_costumes/version.rb
+++ lib/bootstrap_costumes/version.rb
@@ -1,3 +1,3 @@
 module BootstrapCostumes
-  VERSION = '0.1.2'
+  VERSION = '0.2.0'
 end
--- test/dummy/app/helpers/icons_helper.rb
+++ test/dummy/app/helpers/icons_helper.rb
@@ -1,7 +1,31 @@
 module IconsHelper
 
   def icons__right
-    raw '<i class="fad fa-angle-double-right"></i>'
+    raw '<i class="fa-solid fa-angles-right"></i>'
+  end
+
+  def icons__danger
+    raw '<i class="fa-solid fa-circle-exclamation"></i>'
+  end
+
+  def icons__warning
+    raw '<i class="fa-solid fa-circle-exclamation"></i>'
+  end
+
+  def icons__warning
+    raw '<i class="fa-solid fa-circle-exclamation"></i>'
+  end
+
+  def icons__info
+    raw '<i class="fa-solid fa-circle-exclamation"></i>'
+  end
+
+  def icons__help
+    raw '<i class="fa-solid fa-circle-exclamation"></i>'
+  end
+
+  def icons__success
+    raw '<i class="fa-solid fa-circle-exclamation"></i>'
   end
 
 end
