9641a65f8578d339b129483b949edd6db6962983
Simon Kiteley <simon@aflier.com>
2022-09-22 11:38:40 +0100

    Removal of duplicate helper method.

--- lib/generators/bootstrap_costumes/install_generator.rb
+++ lib/generators/bootstrap_costumes/install_generator.rb
@@ -4,6 +4,7 @@ module BootstrapCostumes
       def create_initializer_file
         create_file "config/initializers/initializer.rb", "# Add initialization content here"
         copy_file File.expand_path("../../../../app/helpers/bootstrap_costumes/icons_helper.rb", __FILE__), "app/helpers/icons_helper.rb"
+        copy_file File.expand_path("../../../../app/javascript/controllers/turbo_controller.js", __FILE__), "app/javascript/controllers/turbo_controller.js"
       end
     end
   end
