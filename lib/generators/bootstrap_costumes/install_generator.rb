adf2bd72de67a0147e11de189af24e236dbe12ac
Simon Kiteley <simon@aflier.com>
2022-09-22 11:14:55 +0100

    Removal of duplicate helper method.

--- lib/generators/bootstrap_costumes/install_generator.rb
+++ lib/generators/bootstrap_costumes/install_generator.rb
@@ -3,7 +3,10 @@ module BootstrapCostumes
     class InstallGenerator < Rails::Generators::Base
       def create_initializer_file
         create_file "config/initializers/initializer.rb", "# Add initialization content here"
+        copy_file "app/helpers/icons__helper.rb", File.expand_path("../../../../app/helpers/icons__helper.rb", __FILE__)
       end
     end
   end
 end
+
+# File.expand_path("config/initializers/initializer.rb", __FILE__)
